911c609f2596571ad7fb92cec0ec0b5cfccaa07e
hoangmaihuy <maihuyhoang97@gmail.com>
2025-07-22 09:21:56 +0700

    feat: mill 1.0.1

--- mill-universal-packager/src/io/github/hoangmaihuy/mill/packager/package.scala
+++
@@ -1,8 +0,0 @@
-package io.github.hoangmaihuy.mill
-
-package object packager {
-
-  implicit val subPathRw: upickle.default.ReadWriter[os.SubPath] =
-    upickle.default.readwriter[String].bimap(_.toString, os.SubPath(_))
-
-}
